e44f969e0ffa22a1a8272a1c822a5ab298318798
Tom Klinovsky <tom.klino@gmail.com>
2020-04-03 18:57:45 +0300

    added a detector isLineIntegerKey

--- cmd/key_type_detectors.go
+++ cmd/key_type_detectors.go
@@ -9,6 +9,18 @@ func isLineObjectKey(s string) bool {
   return s[len(s)-1] == ':'
 }
 
+func isLineIntegerKey(s string) bool {
+  valString := strings.Trim(strings.Split(s, ":")[1], "\t ")
+  if len(valString) == 0 {
+    return false
+  }
+  switch valString[0] {
+  case '0','1','2','3','4','5','6','7','8','9':
+    return true
+  }
+  return false
+}
+
 func isLineStringKey(s string) bool {
   valString := strings.Trim(strings.Split(s, ":")[1], "\t ")
   if len(valString) == 0 {
--- cmd/key_type_detectors_test.go
+++ cmd/key_type_detectors_test.go
@@ -10,6 +10,24 @@ func TestIsLineObjectKey(t *testing.T) {
   }
 }
 
+func TestIsLineIntegerKey(t *testing.T) {
+  var integerTests = []struct{
+    in string
+    out bool
+  }{
+    {"key: value", false},
+    {"key: \"value\"", false},
+    {"key:", false},
+    {"key:5", true},
+    {"key: 5", true},
+  }
+  for _, test := range integerTests {
+    if isLineIntegerKey(test.in) != test.out {
+      t.Errorf("the line '%s' is exptected to be %v, but got %v", test.in, test.out, !test.out)
+    }
+  }
+}
+
 func TestIsLineStringKey(t *testing.T) {
   if isLineStringKey("key: value") != true {
     t.Errorf("the line 'key: value' is a string key, but got false")
